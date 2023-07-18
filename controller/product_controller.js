ca4f477ad3491b6755f8e7ffd341d01841407d41
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-19 03:43:11 +0530

    delete product added

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -22,5 +22,16 @@ module.exports.create = async (req,res) =>{
         res.status(400).json({message:`Error in creating Product ${err.message}`});
     }
 }
-
+module.exports.delete = async (req, res) => {
+    try{
+        const deleteProduct = await Product.deleteOne(req.params.id);
+        if(deleteProduct){
+            res.json({message:`Product Deleted `});
+        }else{
+            res.status(404).json({message:`Product not Found`});
+        }
+    }catch(err){
+        res.status().json({message: `Error in Deleting Product ${err.message}`});
+    }
+}
 
