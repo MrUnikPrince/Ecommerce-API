abe0aca5f0e253d57b83b304c9ef0d7a483fc9d8
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-18 23:50:36 +0530

    Schema Done

--- models/product.js
+++ models/product.js
@@ -1,7 +1,19 @@
 const { timeStamp } = require('console');
 const mongoos = require('mongoose');
 const productSchema = mongoos.Schema({
-
-},{timeStamp:true})
+    id: {
+        type: String, // Assuming id is a number
+        required: true // Assuming id is a required field
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String,
+        required: true
+    }
+}, { timeStamp: true })
 
 const Product = mongoos.model('Product', productSchema);
+module.exports = Product;
