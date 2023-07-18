42bcfc378218d50b65994830576155198939b7bf
MrUnikPrince <opuniqueprince@gmail.com>
2023-07-19 03:28:39 +0530

    setting routes and middlewate

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const { log } = require('console');
-const express = require('express');
+// const { log } = require('console');
+const express = require('express'); 
 const port = 8000;
 const db = require('./config/mongoose')
 const app = express();
 
+// middleware
+app.use(express.json());
 
+
+// const routesPath = require('./routes');
+app.use('/', require('./routes'));
 
 
 // Server 
 app.listen(port, (err) => {
     if(err) {
-        log(`Error in running Server ${err}`);
+        console.log(`Error in running Server ${err}`);
     }
-    log(`Server is running ${port}`);
+    console.log(`Server is running ${port}`);
 });
